3b2b0044fc5d3d54c6401b14b84b4b0802ac7e97
Nafisah Mayun <146647721+NafisahMayun@users.noreply.github.com>
2023-11-21 20:50:57 -0800

    moved my weather app to github

---
+++ src/app.js
@@ -0,0 +1,17 @@
+let city = prompt("Enter a city");
+city = city.toLowerCase();
+if (weather[city] !== undefined) {
+  let temperature = weather[city].temp;
+  let humidity = weather[city].humidity;
+  let celsiusTemperature = Math.round(temperature);
+  let fahrenheitTemperature = Math.round((temperature * 9) / 5 + 32);
+
+  alert(
+    `It is currently ${celsiusTemperature}°C (${fahrenheitTemperature}°F) in ${city} with a humidity of ${humidity}%`
+  );
+} else {
+  alert(
+    `Sorry we don't know the weather for this city, try going to https://www.google.com/search?q=weather+${city}`
+  );
+}
+//https://codesandbox.io/s/youthful-dawn-znd3ch?file=/index.html:1859-1896
